4827d346c2014755bafcb8ff4e99a0f4f9b35590
Duy Hà <halac123b@gmail.com>
2024-01-21 06:07:00 +0700

    m_5 21/1

--- 5/shuffle_deck.py
+++ 5/shuffle_deck.py
@@ -11,3 +11,22 @@ headers = {}
 response = requests.request("GET", url, headers=headers, data=payload)
 
 print(response.text)
+
+# Cách 2: không ghi trực tiếp parameter vào URL mà ghi vào querystring
+url = "https://deckofcardsapi.com/api/deck/new/shuffle/"
+
+querystring = {"deck_count": "6"}
+
+headers = {
+    "Cache-Control": "no-cache",
+    "Postman-Token": "dd1d8ca5-7000-21b2-2230-39969d585419",
+}
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+print(response.text)
+# Parse JSON ra dict
+deck = response.json()
+# Access vào key deck_id
+deck_id = deck["deck_id"]
+print(deck_id)
