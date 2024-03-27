0d4a5ca5a38e8641d548f902cc630144eb80d6cf
Duy Hà <94681883+halac123b@users.noreply.github.com>
2024-03-27 13:36:16 +0700

    Update m_3

--- 3/main.py
+++ 3/main.py
@@ -1,6 +1,8 @@
 import dotenv
 
 dotenv.load_dotenv()
+# Options:
+  # override=True: value trong file .env sẽ override lại biến env nếu nó đã có sẵn (default: False)
 
 import os  # provides ways to access the Operating System and allows us to read the environment variables
 
