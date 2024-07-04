309bbf0032a7f89870bca0a95f69377c1a5fcc7d
halac123b <halac123b@gmail.com>
2024-07-05 02:05:56 +0700

    Update m_14

--- 14/main.py
+++ 14/main.py
@@ -5,13 +5,16 @@ setuptools.setup(
     name="aj",
     version="1.0.0",
     python_requires=">=3",
-    install_requires=["package"],
+    install_requires=["package_name"],  # From requirement.txt
     description="Desc for the package",
     author="Duy Ha",
     author_email="halac123game@gmail.com",
-    url="absolute_link",  # URL đến page của package
-    packages=setuptools.find_packages(),
+    url="URL_website",  # URL đến page của package (homepage, document)
+    packages=setuptools.find_packages(),  # List các package, sub-package trong folder để build package hiện tại
+    # Thông thường setuptool chỉ install package gồm các file code, nếu muốn install kèm các format khác (data, text, image) cần setting này
+    # Các file này cần đc list trong file MANIFEST.in và file đc list trong option "package_data"
     include_package_data=True,
+    # List thêm các file k phải code khác để install vào package
     package_data={
         "aj": [
             "aj/static/images/*",
