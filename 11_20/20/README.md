1c6ae036eb59532b99a8b5cbfdb63547b391d857
halac123b <halac123b@gmail.com>
2024-07-18 16:33:56 +0700

    Add m_20

---
+++ 11_20/20/README.md
@@ -0,0 +1,3 @@
+# logging - better logging system
+
+package giúp tổ chức hệ thống log thông tin tốt hơn khi có thể chia log thành nhiều cấp bậc
---
+++ 11_20/20/main.py
@@ -0,0 +1,14 @@
+import logging
+
+# Đặt cấu trúc của câu log cho 1 log level cụ thể
+## level: đag set cho level INFO
+## format: cấu trúc, theo các tên định nghĩa sẵn (time, level, nội dung log)
+## datefmt: format của date dùng cho (asctime)
+format = "%(asctime)s - %(levelname)s - %(message)s"
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%H:%M:%S",
+)
+# Log thông tin ở level INFO
+logging.info("This is an INFO log")
