0eb9c6f3fd4e747dfa9abfd66b0c8e63e31507e5
halac123b <halac123b@gmail.com>
2024-07-18 16:58:48 +0700

    Update m_11

--- 11_20/11/main.py
+++ 11_20/11/main.py
@@ -3,3 +3,6 @@ import time
 # Thời gian từ lúc January 1, 1970
 ## Tùy vào system mà code chạy, kết quả có thể ở dạng milisecond hoặc second
 print(time.time())
+
+# Tạm dừng chương trình trong 2s
+time.sleep(2)
