43eece3a26a4fc26e7ced4730e2481d8b00f6cde
halac123b <halac123b@gmail.com>
2024-07-25 01:11:24 +0700

    Update m_10

--- 1_10/10/main.py
+++ 1_10/10/main.py
@@ -5,3 +5,6 @@ import os
 ## Value is string type
 ## Tác dụng khi modify chỉ tồn tại trong khi chương trình chạy lần đó, chạy xong về như cũ
 os.environ["some_env_var"] = "value"
+
+# str: Lấy ra path đến folder chứa file từ path đến file đó
+print(os.path.dirname("folder/file.txt"))
