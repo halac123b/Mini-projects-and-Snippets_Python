8b9fde1ce70e799b7c9a583402063861e26fa4a3
halac123b <halac123b@gmail.com>
2024-07-26 01:08:40 +0700

    Update m_13

--- 11_20/13/main.py
+++ 11_20/13/main.py
@@ -2,3 +2,6 @@ import platform
 
 # Check loại implemetation của Python trên máy đó (CPython, PyPy)
 print(platform.python_implementation())
+
+# String: tên version Python hiện tại
+print(platform.python_version())
