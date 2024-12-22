edb4565931dce6810372ffbb04e118aaf1b81148
halac123b <halac123b@gmail.com>
2024-12-23 02:24:51 +0700

    Update m_36

--- 31_40/36/main.py
+++ 31_40/36/main.py
@@ -1,1 +1,14 @@
 import mailtrap
+
+mail = mailtrap.Mail(
+    sender=mailtrap.Address(email="hello@demomailtrap.com", name="Mailtrap Test"),
+    to=[mailtrap.Address(email="halac123b@gmail.com")],
+    subject="You are awesome!",
+    text="Congrats for sending test email with Mailtrap!",
+    category="Integration Test",
+)
+
+client = mailtrap.MailtrapClient(token="********4760")
+response = client.send(mail)
+
+print(response)
