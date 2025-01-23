9fe1c00fb3c0b20e2903b2f019c79d2df5e75549
halac123b <halac123b@gmail.com>
2025-01-23 18:41:32 +0700

    Update m_35

--- 31_40/35/app.py
+++ 31_40/35/app.py
@@ -1,3 +1,13 @@
 import tkinter
 import os
 
+class ImageToPDFConverter:
+    def __init__(self, root):
+        self.root = root
+        self.image_paths = []
+        self.output_pdf_name = tkinter.StringVar()
+        self.selected_images_listbox = tkinter.Listbox(self.root, selectmode=tkinter.MULTIPLE)
+
+    def inintialize_UI(self):
+        title_label = tkinter.Label(self.root, text="Image to PDF Converter", font=("Helvetica", 16, "bold"))
+        title_label.pack()
